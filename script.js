d01c2fbcde11287add690f9ee1965027e44faac3
Leo Mikko Uson <105025585+Kuroe27@users.noreply.github.com>
2023-02-06 22:04:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,57 +2,47 @@ const form = document.getElementById('card')
 const input = document.getElementById('submitInput')
 const lit = document.querySelector('.list2')
 const box = document.getElementById('check')
-const list = document.querySelector("ul")
-const items = list.querySelectorAll("li")
-
-
-
-
 
 form.addEventListener('submit', addItem)
-del.addEventListener('click', removeItem)
-check.addEventListener('change', changeItem)
-
-
-
 function addItem(e) {
     e.preventDefault()
     const newItem = input.value
 
-    var li = document.createElement('li')
-    li.className = 'items'
 
-    var ints = document.createElement('input')
-    ints.type = 'text'
-    ints.className = 'lis'
-
-    var check = document.createElement('input')
-    check.type = 'checkbox'
-    check.className = 'cre'
-    li.appendChild(check)
-
-    var text = document.createElement('span')
-    text.className = 'text'
-    text.appendChild(document.createTextNode(newItem))
-    li.appendChild(text)
-    lit.appendChild(li)
-    li.appendChild(ints)
-
-    var del = document.createElement('button')
-    del.className = 'remove'
-    del.appendChild(document.createTextNode('X'))
-    li.appendChild(del)
-
-    items = list.querySelectorAll("li")
-    items.forEach((item, index) => {
-        const number = index + 1;
-        const h2 = item.previousElementSibling;
-        h2.innerHTML = `${number}. ${h2.innerHTML}`;
-    });
-
-    input.value = ""
+    if (input.value !== ''){
+        var li = document.createElement('li')
+        li.className = 'items'
+    
+        var ints = document.createElement('input')
+        ints.type = 'text'
+        ints.className = 'lis'
+    
+        var check = document.createElement('input')
+        check.type = 'checkbox'
+        check.className = 'cre'
+        li.appendChild(check)
+    
+        var text = document.createElement('span')
+        text.className = 'text'
+        text.appendChild(document.createTextNode(newItem))
+        li.appendChild(text)
+        lit.appendChild(li)
+        li.appendChild(ints)
+    
+        var del = document.createElement('button')
+        del.className = 'remove'
+        del.appendChild(document.createTextNode('X'))
+        li.appendChild(del)
+    
+        del.addEventListener('click', removeItem)
+        check.addEventListener('change', changeItem)
+    
+        input.value = ''
+    }else{
+        alert("ads")
+    }
+   
 }
-
 
 function changeItem(e) {
     var text = e.target.parentElement.querySelector('.text')
